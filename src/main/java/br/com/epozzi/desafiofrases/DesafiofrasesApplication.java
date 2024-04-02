f163fa8275b9358a254658c433c44e544a27f1a3
Eduardo Pozzi <eduardoampozzi@gmail.com>
2024-04-02 14:58:52 -0300

    Primeiro commit iniciando o projeto

---
+++ src/main/java/br/com/epozzi/desafiofrases/DesafiofrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.epozzi.desafiofrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiofrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiofrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/epozzi/desafiofrases/DesafiofrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.epozzi.desafiofrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiofrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
